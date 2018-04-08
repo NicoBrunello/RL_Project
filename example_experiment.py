d45cbb56fe5189ca467cb7f6a9a0b16ad05c9844
NicoBrunello <nicolo.brunello@mail.polimi.it>
2018-04-08 16:40:25 -0700

    Implemented REINFORCE, but policy update is not working

--- example_experiment.py
+++ example_experiment.py
@@ -1,25 +1,40 @@
 from gym_torcs import TorcsEnv
 from sample_agent import Agent
 import numpy as np
+import matplotlib as pl
 
 vision = False
-episode_count = 500
+episode_count = 3
 max_steps = 100
 reward = 0
 done = False
 step = 2
-theta = np.ndarray(shape=(1,8), dtype=(float))
+# Theta represent the policy
+theta = np.ndarray(shape=(8,3), dtype=(float))
+#Learning rate
+alpha=0.0001
+
+def compute_gradient(a_s_vector, av_theta, J ):
+    delta_theta =[[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]]
+    for i in range(len(a_s_vector)):
+        action=a_s_vector[i][1] - av_theta
+        state = a_s_vector[i][0]
+        delta_theta = delta_theta + (np.multiply(action[0] - av_theta[0],  state)  / (0.1*0.1))
+    baseline= ((delta_theta**2) * J)/(delta_theta**2)
+    gradient = delta_theta * (J-baseline**8)
+    return gradient
 
 
 # Generate a Torcs environment
 print ("Creating Torcs environment")
 env = TorcsEnv(vision=vision, throttle=False)
 print("Torcs env created--------------------")
-agent = Agent(1)  # steering onlyt_n = np.array(shape=(1,8), dtype=(float))
+agent = Agent(3)  # now we use steering only, but we might use throttle and gear
 
 #Init theta vector
 
-theta=np.random.normal(1, 0.1,(1,8))
+theta=np.random.normal(1, 0.1,(3,8))
+performance = np.array([0])
 
 print("TORCS Experiment Start.")
 for i in range(episode_count):
@@ -31,30 +46,41 @@ for i in range(episode_count):
     else:
         ob = env.reset()
 
-
     total_reward = 0.
+    states = np.array([[0,0,0]])
+    J= 0;
 
     for j in range(max_steps):
 
-        action = agent.act(ob, reward, done, vision, theta)
+        action, raw_action, av_theta, ob_theta = agent.act(ob, reward, done, vision, theta)
         ob, reward, done, _ = env.step(action)
-        print("\n-------------------------------------------------------")
-        #print(ob)
-        print("\n-------------------------------------------------------")
-        total_reward += reward
 
+        #print("\n-------------------------------------------------------")
+        #print(ob)
+        #print("\n-------------------------------------------------------")
+        total_reward += reward
+        ## update the vector of trajectories
+        states = np.append(states, [[ob_theta, raw_action, reward]], axis=0 )
+        ## update performance
+        J= J + (0.99**j) * (reward) 
         step += 1
         if done:
             break
 
+    performance = np.append(performance, [J])
+    gradient = compute_gradient(states, av_theta, J)
+    theta = theta + alpha * gradient
+    print("theta ------------->"+str(theta))
+    #print(str(J))
     print("TOTAL REWARD @ " + str(i) +" -th Episode  :  " + str(total_reward))
     print("Total Step: " + str(step))
+    print("----------------------------------------   J = " + str(J) + " L = "+ str(performance.size))
     print("")
+
+pl.pyplot.plot(range(episode_count + 1) ,performance)
+pl.pyplot.show()
 
 env.end()  # This is for shutting down TORCS
 print("Finish.")
 
-
-#def compute_gradient(a_s_vector, t_n, max_steps )
-#pi_theta = np.random.multiariate_normal(t_n, cov)
 
--- sample_agent.py
+++ sample_agent.py
@@ -9,7 +9,7 @@ class Agent(object):
 
     def act(self, ob, reward, done, vision_on, theta):
 
-        cov= np.identity(8) *0.1
+        cov= np.identity(3) *0.1
         #print("ACT!")
 
         # Get an Observation from the environment.
@@ -24,22 +24,34 @@ class Agent(object):
         else:
             focus, speedX, speedY, speedZ, opponents, rpm, track, wheelSpinVel, vision = ob
         
-        #Preparing the state 
+        #Preparing the state
+        # Scale rpm between [0-1] 
+        n_rpm = np.tanh(rpm)
+        #Scale the wheelSpinVel vector
+        n_wheelSpinVel = np.tanh(wheelSpinVel)
 
-
-        #Get the average action theta * ob
-        # Values of ob must be prepared
-        av_theta =[np.dot(theta[0,1],focus[0]),
-                    np.dot(theta[0,2],speedX),
-                    np.dot(theta[0,2],speedY),
-                    np.dot(theta[0,3],speedZ),
-                    np.dot(theta[0,4],opponents[0]),
-                    np.dot(theta[0,5],rpm),
-                    np.dot(theta[0,6],track[0]),
-                    np.dot(theta[0,7],wheelSpinVel[0]) ] 
-        #Sample the action from a gaussian distribution with mean value av_theta
-        action =np.random.multivariate_normal(av_theta, cov)
-        #return the action
-        print(np.tanh(action))
+        ob_theta1 = [focus[0], speedX, speedY, speedZ, opponents[0], rpm, track[0], wheelSpinVel[0]]
+        ob_theta2 = [focus[0], speedX, speedY, speedZ, opponents[0], rpm, track[0], wheelSpinVel[0]]
+        ob_theta3 = [focus[0], speedX, speedY, speedZ, opponents[0], rpm, track[0], wheelSpinVel[0]]
         
-        return np.tanh(action)
+        for i in range(self.dim_action):
+            theta[i] =  (theta[i]-min(theta[i]))/(max(theta[i])-min(theta[i])) -0.5
+        #Get the average action theta * ob
+        #todo: transform vectors in values,  now just to try out the code I take the first value vector feature
+        av_theta =  [np.dot(theta[0],ob_theta1),
+                    np.dot(theta[1],ob_theta2),
+                    np.dot(theta[2],ob_theta3)]
+        print("av_theta--> "+ str(av_theta))
+        #av_theta =np.dot(theta[0,1],focus[0]) + np.dot(theta[0,2],speedX) + np.dot(theta[0,2],speedY) + np.dot(theta[0,3],speedZ) + np.dot(theta[0,4],opponents[0]) + np.dot(theta[0,5],n_rpm) + np.dot(theta[0,6],track[0]) + np.dot(theta[0,7],n_wheelSpinVel[0]) 
+        #Normalize av_theta 
+        av_theta =  (av_theta-min(av_theta))/(max(av_theta)-min(av_theta)) -0.5
+        
+        #Sample the action from a gaussian distribution with mean equals to av_theta
+        action =np.random.multivariate_normal(av_theta, cov)
+        action =  (action-min(action))/(max(action)-min(action)) -0.5
+        #return the action
+        print(np.tanh(action))     
+        return (np.tanh(action), action, av_theta, [ob_theta1, ob_theta2, ob_theta3])
+
+
+
